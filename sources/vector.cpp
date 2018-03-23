73e71763a9ba1b6e2e44e1a9a7181cf6652772b1
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2018-03-23 14:59:09 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -13,25 +13,32 @@ vector_t::vector_t(vector_t const& other) {
   size_ = other.size_;
   capacity_ = other.capacity_;
   elements_ = new int[capacity_];
-  for (std::size_t i = 0; i < size_; i++) {
-    elements_[i] = other.elements_[i];
-  }
+  for (unsigned int i = 0; i < size_; i++) elements_[i] = other.elements_[i];
 }
 
 vector_t& vector_t::operator=(vector_t const& other) {
   if (this != &other) {
     delete[] elements_;
-    size_ = other.size_;
-    capacity_ = other.capacity_;
-    elements_ = new int[capacity_];
-    for (std::size_t i = 0; i < size_; i++) {
-      elements_[i] = other.elements_[i];
-    }
+    this->size_ = other.size_;
+    this->capacity_ = other.capacity_;
+    this->elements_ = new int[capacity_];
+    for (unsigned int i = 0; i < size_; i++) elements_[i] = other.elements_[i];
   }
   return *this;
 }
 
-bool vector_t::operator==(vector_t const& other) const { return false; }
+bool vector_t::operator==(vector_t const& other) const {
+  if (this->size_ == other.size_) {
+    for (int i = 0; i < size_; i++) {
+      if (this->elements_[i] != other.elements_[i]) {
+        return false;
+        break;
+      }
+    }
+    return true;
+  } else
+    return false;
+}
 
 vector_t::~vector_t() {
   delete[] elements_;
@@ -39,9 +46,15 @@ vector_t::~vector_t() {
   capacity_ = 0;
 }
 
-std::size_t vector_t::size() const { return size_; }
+std::size_t vector_t::size() const {
+  return size_;
+  return 0;
+}
 
-std::size_t vector_t::capacity() const { return capacity_; }
+std::size_t vector_t::capacity() const {
+  return capacity_;
+  return 0;
+}
 
 void vector_t::push_back(int value) {
   if (size_ == 0) {
@@ -66,6 +79,7 @@ void vector_t::push_back(int value) {
 }
 
 void vector_t::pop_back() {
+  size_--;
   if (size_ == 0 || size_ * 4 == capacity_) {
     int* mas;
     mas = new int[size_];
@@ -86,4 +100,7 @@ int& vector_t::operator[](std::size_t index) { return elements_[index]; }
 
 int vector_t::operator[](std::size_t index) const { return elements_[index]; }
 
-bool operator!=(vector_t const& lhs, vector_t const& rhs) { return true; }
+bool operator!=(vector_t const& lhs, vector_t const& rhs) {
+  if (lhs == rhs) return false;
+  return true;
+}
