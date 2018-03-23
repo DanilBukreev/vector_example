c7e34ca07f232ce96a7771b8e2771ba9d3da08ae
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2018-03-23 11:35:45 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -2,9 +2,10 @@
 class vector_t
 {
 private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+	int * elements_; // указатлеь на динамический массив 
+	std::size_t size_; // количсетво элементов 
+	std::size_t capacity_; // обьем памяти который мы выделили 
+                               // если capacity == size то делаем перераспределение памяти 
 public:
 	vector_t();
 	vector_t(vector_t const & other);
@@ -14,8 +15,8 @@ public:
 	std::size_t size() const;
 	std::size_t capacity() const;
 
-	void push_back(int value);
-	void pop_back();
+	void push_back(int value); //положить эл в конец
+	void pop_back(); //достать элемент (удалить)
 
 	int & operator [](std::size_t index);
 	int operator [](std::size_t index) const;
